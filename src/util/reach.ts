108d7ef30633507ad522f0b58da996c758c74cd1
Leonardo Ascione <leonardo.ascione@gmail.com>
2023-06-21 17:34:31 +0200

    feat: add reachAndResolve method

--- src/util/reach.ts
+++ src/util/reach.ts
@@ -1,6 +1,6 @@
 import { forEach } from 'property-expr';
 import type Reference from '../Reference';
-import type { InferType, ISchema } from '../types';
+import type { Ancester, InferType, ISchema } from '../types';
 import type { Get } from 'type-fest';
 
 export function getIn<C = any>(
@@ -12,16 +12,22 @@ export function getIn<C = any>(
   schema: ISchema<any> | Reference<any>;
   parent: any;
   parentPath: string;
+  from: Ancester<C>[];
+  value: any;
 } {
-  let parent: any, lastPart: string, lastPartDebug: string;
+  let parent: any,
+    parentSchema: any,
+    lastPart: string,
+    lastPartDebug: string,
+    from: Ancester<C>[] = [];
 
   // root path: ''
-  if (!path) return { parent, parentPath: path, schema };
+  if (!path) return { parent, parentPath: path, schema, from, value };
 
   forEach(path, (_part, isBracket, isArray) => {
     let part = isBracket ? _part.slice(1, _part.length - 1) : _part;
 
-    schema = schema.resolve({ context, parent, value });
+    schema = schema.resolve({ context, parent, value, from });
 
     let isTuple = schema.type === 'tuple';
     let idx = isArray ? parseInt(part, 10) : 0;
@@ -54,15 +60,17 @@ export function getIn<C = any>(
         );
 
       parent = value;
+      parentSchema = schema;
       value = value && value[part];
       schema = schema.fields[part];
+      from = [{ schema: parentSchema, value: parent }, ...from];
     }
 
     lastPart = part;
     lastPartDebug = isBracket ? '[' + _part + ']' : '.' + _part;
   });
 
-  return { schema, parent, parentPath: lastPart! };
+  return { schema, parent, parentPath: lastPart!, from, value };
 }
 
 function reach<P extends string, S extends ISchema<any>>(
@@ -74,6 +82,23 @@ function reach<P extends string, S extends ISchema<any>>(
   | Reference<Get<InferType<S>, P>>
   | ISchema<Get<InferType<S>, P>, S['__context']> {
   return getIn(obj, path, value, context).schema as any;
+}
+
+export function reachAndResolve<P extends string, S extends ISchema<any>>(
+  obj: S,
+  path: P,
+  value?: any,
+  context?: any,
+):
+  | Reference<Get<InferType<S>, P>>
+  | ISchema<Get<InferType<S>, P>, S['__context']> {
+  const reachedSchema = getIn(obj, path, value, context);
+  return reachedSchema.schema.resolve({
+    context,
+    parent: reachedSchema.parent,
+    value: reachedSchema.value,
+    from: reachedSchema.from,
+  }) as any;
 }
 
 export default reach;
